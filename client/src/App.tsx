df91b4f3d9d80e668c49b7824034163c651c6cb1
eunbin park <eunbinpark@eunbinui-MacBookPro.local>
2022-06-20 21:11:53 +0900

    routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useState } from "react";
+import Landing from "./pages/Landing";
+import MemoP from "./pages/MemoP";
+import NewMemo from "./pages/NewMemo";
+import NewPaper from "./pages/NewPaper";
+import Paper from "./pages/Paper";
 
 function App() {
+  const [title, setTitle] = useState<string>("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/memo" element={<MemoP />} />
+        <Route path="/newmemo" element={<NewMemo />} />
+        <Route path="/newpaper" element={<NewPaper />} />
+        <Route path="/paper" element={<Paper />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- client/src/pages/Landing.tsx
+++ client/src/pages/Landing.tsx
@@ -1,7 +1,27 @@
+import styled from "styled-components";
+
+const LandingWrap = styled.div`
+  width: 100vw;
+  height: 100vh;
+  position: relative;
+  background-color: blue;
+
+  button {
+    width: 300px;
+    height: 50px;
+    border-radius: 20%;
+    background-color: transparent;
+    border: 2px solid #fff;
+    color: blue;
+    position: absolute;
+    bottom: 200px;
+  }
+`;
+
 export default function Landing() {
   return (
-    <div>
-      
-    </div>
-  )
+    <LandingWrap>
+      <button>babble site 시작하기</button>
+    </LandingWrap>
+  );
 }
